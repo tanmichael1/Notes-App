3dc255565562602f6bab830771d9421d4e88d5a7
tanmichael1 <michaelrstan@gmail.com>
2021-10-06 16:07:56 +1300

    Add edit notes functionality

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Note from "./Note";
+import Editable from "./Editable";
 
-export default ({ notes, onDelete = () => {} }) => (
+export default ({
+  notes,
+  onNoteClick = () => {},
+  onEdit = () => {},
+  onDelete = () => {},
+}) => (
   <ul>
-    {notes.map(({ id, task }) => (
+    {notes.map(({ id, editing, task }) => (
       <li key={id}>
-        <Note onDelete={onDelete.bind(null, id)} task={task} />
+        <Note onClick={onNoteClick.bind(null, id)}>
+          <Editable
+            editing={editing}
+            value={task}
+            onEdit={onEdit.bind(null, id)}
+          />
+          <button onClick={onDelete.bind(null, id)}>x</button>
+        </Note>
       </li>
     ))}
   </ul>
