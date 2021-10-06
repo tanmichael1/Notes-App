9b25afd26c9c63893ff04f85c3ea3ad4e6f55469
tanmichael1 <michaelrstan@gmail.com>
2021-10-06 15:01:25 +1300

    Add Note function - basic note adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Notes from "./components/Notes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { v4 as uuidv4 } from "uuid";
+
+// if (process.env.NODE_ENV !== "production") {
+//   React.Perf = require("react-addons-perf");
+// }
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuidv4(),
+          task: "Learn React",
+        },
+        {
+          id: uuidv4(),
+          task: "Do laundry",
+        },
+      ],
+    };
+  }
+
+  addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
+    this.setState({
+      notes: this.state.notes.concat([
+        {
+          id: uuidv4(),
+          task: "New task",
+        },
+      ]),
+    });
+  };
+  render() {
+    const { notes } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+
+        <Notes notes={notes} />
+      </div>
+    );
+  }
 }
-
-export default App;
